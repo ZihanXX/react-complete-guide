b65c4843221611059d958f9fd73e8abf228cda42
ZihanXX <zihanxxie@gmail.com>
2017-12-28 23:29:49 -0800

    44

--- src/App.js
+++ src/App.js
@@ -23,12 +23,20 @@ class App extends Component {
     });
   }
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: 'Chris H', age: 24},
+        {name: event.target.value, age: 3}
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I am a React App.</h1>
         <button
-          // click 不同位置会结果不一样
           onClick={this.switchNameHandler.bind(this, "Zihan")}
         >Switch Name</button>
         <Person
@@ -38,8 +46,9 @@ class App extends Component {
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          // click 不同位置会结果不一样
           click={this.switchNameHandler.bind(this, "Little Cute")}
+          // here, change the name
+          changed={this.nameChangedHandler}
         >My Hobbies: Being silly.</Person>
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,11 +3,16 @@ import React from 'react';
 const person = (props) => {
   return (
     <div>
+
       <p
         // 用这一行来传递method
         onClick={props.click}
       >I am {props.name}! I am {props.age} years old.</p>
+
       <p>{props.children}</p>
+
+      <input type="text" onChange={props.changed} value={props.name} />
+
     </div>
   );
 }
